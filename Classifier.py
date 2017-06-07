076284761bb42613d4ad92ada3d483f251e6238f
Salim M'jahad <salimmjahad@dyn-209-2-208-78.dyn.columbia.edu>
2017-06-07 18:38:53 -0400

    Added Data Generator skeleton

--- Classifier.py
+++ Classifier.py
@@ -1,4 +1,5 @@
 from sklearn.model_selection import cross_val_score
+import numpy as np
 
 """
     Classifier Class
@@ -7,6 +8,10 @@ from sklearn.model_selection import cross_val_score
     PS: This class will later be renamed to LinearModel and will support both
         regression models and linear classifiers for both continuous and binary
         features
+
+        "feature1", fea
+
+        features.feature1(w_i)
 """
 
 class Classifier:
@@ -16,12 +21,18 @@ class Classifier:
         @param model - Model to train on
         @param feature_list - List of features to train on
     """
-    def __init__(self, model, feature_list):
+    def __init__(self, model, feature_list, n_cells):
 
         self.model = model
         self.feature_list = feature_list
+        self.n_cells = n_cells
 
-        self.trained_classifiers = [LinearClassifier(model, impulse_feature) for impulse_feature in self.feature_list]
+        # list of cells. each cell is a list of features. each feature is a linear classifier
+        self.trained_classifiers = [[LinearClassifier(model.get_cell(cell), impulse_feature) for impulse_feature in self.feature_list] for
+                                    cell in range(n_cells)]
+
+        self.scores = np.array([ [ feature.score for feature in cell_clf ] for cell_clf in self.trained_classifiers ])
+
 
 
 class LinearClassifier:
@@ -36,12 +47,12 @@ class LinearClassifier:
         self.model = model
         self.impulse_feature = impulse_feature
 
-        self.trained_linear_classifier = self.train()
+        self.score = self.get_score()
 
-    def train(self):
+    def get_score(self):
 
 
-        scores = cross_val_score(clf, iris.data, iris.target, cv=5)
+        scores = cross_val_score
 
 
         pass
---
+++ feature_functions.py
@@ -0,0 +1,7 @@
+
+def feature1(w):
+
+    if w == '(' or w == ')':
+        return 1
+    else:
+        return 0
